12636392bd58daab6421ebb088c8761519f59911
Grace Nieboer <grace.nieboer@outlook.com>
2023-02-11 15:48:09 -0500

    practice problem solving

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+for (let i=0; i<array.length; i++) {
+  let complement = target - array[i]
+  for (let j=i+1; j<array.length; j++) {
+    if (complement === array[j]) {
+      return true
+    }
+  } 
 }
+return false
+}
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+  function takes two arguments, an array and a target
+  integer which is the goal in which two integers in the array 
+  is equal to.
+
+  the function should  if any 2 of the numbers 
+  in the array = the target integer, return true
+ 
+  ///at each number in the array, look through the array to see if
+  that number + another number = the target.
+
+  forEach (element in the array => add the others individually to the number to see if it equals 
+    the target)
+    if so, return true.
+
+    element => 
+
+
+
 */
 
 /*
@@ -29,6 +56,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([20, 30, 4, 800], 100));
 }
 
 module.exports = hasTargetSum;
